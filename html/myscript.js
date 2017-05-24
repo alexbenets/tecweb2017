e37b4399cc8b848572711356f66cafa226393928
merym <mery.morra@gmail.com>
2017-05-24 17:52:28 +0200

    Add files via upload

---
+++ html/myscript.js
@@ -0,0 +1,19 @@
+function first(){
+				var x=document.getElementsByClassName("faqAnswer");
+				n=x.length;
+				for (i=0; i<n; i++){
+					hide(x[i]);
+				}
+			}
+			
+function hide(element){
+				element.style.display='none';
+			}
+			
+function special(id){
+				if(document.getElementById(id).style.display=="none"){
+					document.getElementById(id).style.display="block";
+				}
+				else
+					document.getElementById(id).style.display="none";
+			}
