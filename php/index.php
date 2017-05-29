31691b70c845ba7f9eed38235240387cc89f079d
merym <mery.morra@gmail.com>
2017-05-29 14:25:05 +0200

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,12 +1,12 @@
 <?php
 	$fileBegin=fopen("testi/homeStart.txt", r) or die ("unable to open file");
-	echo fread($fileBegin, filesize("testi/homeStart.txt");
+	echo fread($fileBegin, filesize("testi/homeStart.txt"));
 	fclose($fileBegin);
-	$news=("breve riassunto della prima notizia"=>"12/5/16", "I love the ocean, why dont you go get eaten by a shark"=>"12/5/16", 
-		"my dearest light, the days are hopeless"=>"5/4/16");
+	$news=["breve riassunto della prima notizia"=>"12/5/16", "I love the ocean, why dont you go get eaten by a shark"=>"12/5/16", 
+		"my dearest light, the days are hopeless"=>"5/4/16"];
 	foreach($news as $x=>$val)
 		echo"<dt><a href=\"1.html\">$val</a></dt><dd>$x</dd>";
-	$fileEnd=("testi/homeFinish.txt", r) or die ("unable to open file");
-	echo fread($fileEnd, filesize("testi/homeFinish.txt");
+	$fileEnd=fopen("testi/homeFinish.txt", r) or die ("unable to open file");
+	echo fread($fileEnd, filesize("testi/homeFinish.txt"));
 	fclose($fileEnd);
 ?>
